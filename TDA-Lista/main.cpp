dc1fb89faee5e447e88dd7ad174f31302c625d86
Matthias Clein <m.cleinespinoza@gmail.com>
2021-10-05 19:24:52 -0300

    test TDA Lista v1

--- TDA-Lista/main.cpp
+++ TDA-Lista/main.cpp
@@ -2,42 +2,34 @@
 #include "lista.h"
 
 int main(){
-    printf("TDA de Lista\n");
+    printf("Test del TDA de Lista\n");
     Lista lista = NULL;
     anula(lista);
     if(vacia(lista)){
         printf("La lista está vacía!\n");
     }
 
+    // Lista lista = NULL;
+    // inserta(10, 1, lista);
+    // inserta(20, 2, lista);
+    // inserta(30, 3, lista);
+    // inserta(40, 4, lista);
+    // inserta(50, 5, lista);
+    // inserta(60, 2, lista);
+
     // if(vacia(lista)){
-    //     printf("La lista está vacía\n");
+    //     printf("La lista está vacía!\n");
     // } else {
-    //     printf("Hay elementos\n");
+    //     printf("La lista no está vacía!\n");
     // }
 
-    // inserta(5, 1, lista);
-    // inserta(10, 2, lista);
-    // inserta(20, 3, lista);
-    // inserta(30, 4, lista);
-    // inserta(40, 5, lista);
-    // inserta(50, 6, lista);
+    // imprimeLista(lista);
 
-    // anula(lista);
-    
-    // if(vacia(lista)){
-    //     printf("La lista está vacía\n");
-    // } else {
-    //     printf("Hay elementos\n");
-    // }
-
-    //imprimeLista(lista);
-
-    // int posicion = localiza(400, lista);
+    // int posicion = localiza(30, lista);
     // if(posicion == fin(lista)){
     //     printf("El elemento no se encuentra.\n");
     // } else {
     //     printf("El elemento se encuentra en la posición %d\n", posicion);
     // }
-
     return 0;
 }
